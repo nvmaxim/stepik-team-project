ea2326cc923b53790a37d7947ff44171887be1af
nvmaxim <123912180+nvmaxim@users.noreply.github.com>
2026-04-16 11:07:17 +0300

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+# является ли строка s палиндромом то есть перевертыш
+
+s = "privet_drug"
+
+left = 0
+rihgt = len(s) - 1
+is_palindrome = True
+
+while left < right:
+  if s[left] != right:
+    is_palindrome = False
+    break
+  left += 1
+  right -= 1
+
+print(is_palindrome)
+
+
