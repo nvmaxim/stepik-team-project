f136ca93d29d0f93ab85f7c747846ce96a79bce9
nvmaxim <m.novikov2050@yandex.ru>
2026-04-16 11:47:43 +0300

    update func greet

--- polyakov-def.py
+++ polyakov-def.py
@@ -1,2 +1,15 @@
-def greet():
-    return "Polyakov was here"
+# вношу изменения в функцию greet - string (проверяем уникальность строки)
+
+stirng = "ветка manual-conflict"
+
+
+def greet(s):
+    seen = []
+    for element in s:
+        if element not in seen:
+            return False
+        seen += [element]
+    return True
+
+
+print(greet(stirng))
