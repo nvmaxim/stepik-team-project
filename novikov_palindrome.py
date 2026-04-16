e7145968892ca815433023526791601da4c812cd
Polyakovoa <polyakovoa@mail.ru>
2026-04-16 21:26:32 +0400

    Изменена переменная s , исправлены ошибки

--- novikov_palindrome.py
+++ novikov_palindrome.py
@@ -1,18 +1,18 @@
 # является ли строка s палиндромом то есть перевертыш
 # нужно внести правку в значение переменнйо s, чтобы palindome был в итоге true
 
-s = "privet_drug"
+s = "radar" # Теперь s - палиндром
 
 left = 0
-rihgt = len(s) - 1
+right = len(s) - 1 # исправлена опечатка rihgt вместо right
 is_palindrome = True
 
 while left < right:
-  if s[left] != right:
-    is_palindrome = False
-    break
-  left += 1
-  right -= 1
+    if s[left] != s[right]: # исправлена ошибка в условии: s[left] != right (сравнение символа с индексом)
+        is_palindrome = False
+        break
+    left += 1
+    right -= 1
 
 print(is_palindrome)
 
